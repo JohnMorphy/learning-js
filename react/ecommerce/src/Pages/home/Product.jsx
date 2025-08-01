2d28e92a5f3b02b318e6f97063e6d9e4d3373fa1
Jan Kaiser <jan.kaiser@3qcode.com>
2025-08-01 10:33:24 +0200

    integration tests for Product component

--- react/ecommerce/src/Pages/home/Product.jsx
+++ react/ecommerce/src/Pages/home/Product.jsx
@@ -38,6 +38,7 @@ export default function Product({ product, loadCart }) {
         <div className="product-container">
             <div className="product-image-container">
                 <img className="product-image"
+                    data-testId="product-image"
                     src={product.image} />
             </div>
 
@@ -47,6 +48,7 @@ export default function Product({ product, loadCart }) {
 
             <div className="product-rating-container">
                 <img className="product-rating-stars"
+                    data-testid="product-rating-stars-image"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                 <div className="product-rating-count link-primary">
                     {product.rating.count}
