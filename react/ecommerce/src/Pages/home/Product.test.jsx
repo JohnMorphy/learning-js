2d8066c98252db30539a325cd602aa2048cef0ff
Jan Kaiser <jan.kaiser@3qcode.com>
2025-08-01 10:45:53 +0200

    used beforeEach to remove duplication

--- react/ecommerce/src/Pages/home/Product.test.jsx
+++ react/ecommerce/src/Pages/home/Product.test.jsx
@@ -1,10 +1,20 @@
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import Product from "./Product";
 import { render, screen } from '@testing-library/react';
+import axios from "axios";
+import userEvent from '@testing-library/user-event'
+
+// create fake axios - that will not contact the backend
+vi.mock('axios');
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+
+    let product;
+    let loadCart;
+
+    // test hook
+    beforeEach(() => {
+        product = {
             "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
             "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -16,7 +26,11 @@ describe('Product component', () => {
             "keywords": ["socks", "sports", "apparel"]
         };
 
-        const loadCart = vi.fn();
+        loadCart = vi.fn();
+    })
+
+    it('displays the product details correctly', () => {
+
 
         render(<Product loadCart={loadCart} product={product} />);
 
@@ -39,6 +53,42 @@ describe('Product component', () => {
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
-
     })
+
+    it('adds a product to a cart', async () => {
+        {
+            const product = {
+                "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+                "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+                "rating": {
+                    "stars": 4.5,
+                    "count": 87
+                },
+                "priceCents": 1090,
+                "keywords": ["socks", "sports", "apparel"]
+            };
+
+            const loadCart = vi.fn();
+
+            render(<Product loadCart={loadCart} product={product} />);
+
+            const user = userEvent.setup();
+            user.click();
+
+            // need to mock the add to cart button onClick
+            const addToCartButton = screen.getByTestId('add-to-cart-button');
+            await user.click(addToCartButton);
+
+            expect(axios.post).toHaveBeenCalledWith('api/cart-items',
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 1,
+                }
+            );
+
+            expect(loadCart).toHaveBeenCalled();
+        }
+    })
+
 })
